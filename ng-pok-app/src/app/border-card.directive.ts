a0cd7fd8fb4b115df4fe763157d7928205904b81
zahaira <zahairajaada@gmail.com>
2023-07-13 15:37:11 +0100

    ameliorer les directives d'attribut

--- ng-pok-app/src/app/border-card.directive.ts
+++ ng-pok-app/src/app/border-card.directive.ts
@@ -5,16 +5,20 @@ import { Directive, ElementRef , HostListener, Input} from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number=180;
+   
   constructor(private el: ElementRef) { 
-    this.setHeight(180);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
   @Input('pkmnBorderCard') borderColor: string;
   @HostListener('mouseenter') onMouseEnter(){
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
   @HostListener('mouseleave') onMouseLeave(){
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
   
   setHeight(height: number){
