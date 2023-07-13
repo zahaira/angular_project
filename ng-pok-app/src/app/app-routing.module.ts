97dc912da3726a6b3f3945a2936e30a9ea462287
zahaira <zahairajaada@gmail.com>
2023-07-13 18:09:21 +0100

    routes

--- ng-pok-app/src/app/app-routing.module.ts
+++ ng-pok-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailPokemonComponentComponent } from './detail-pokemon-component/detail-pokemon-component.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'pokemons', component: ListPokemonComponent},
+ { path: 'pokemon/:id', component: DetailPokemonComponentComponent},
+ { path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+ {path:'**', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
